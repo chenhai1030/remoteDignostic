ccf72fd35ac433ab6a75192217f94a83343cf564
chenhai <chenhai@fun.tv>
2019-04-11 15:12:25 +0800

    add for collectstatic

--- RemoteDignostic/settings.py
+++ RemoteDignostic/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_FINDERS = (
+        "django.contrib.staticfiles.finders.FileSystemFinder",
+        "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
